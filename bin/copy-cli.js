2e5e765268c51ac32634a77d2b37bd87ce33e1e2
Richadson S Junior <66044289+Richadsonjr@users.noreply.github.com>
2025-05-31 12:32:43 -0300

    Nova Versão

--- bin/copy-cli.js
+++ bin/copy-cli.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const fs = require('fs');
 const path = require('path');
 
@@ -11,18 +10,4 @@ fs.copyFile(sourceFile, destFile, (err) => {
     } else {
         console.log('Arquivo cli.js copiado com sucesso.');
     }
-=======
-const fs = require('fs');
-const path = require('path');
-
-const sourceFile = path.resolve(__dirname, 'cli.js');
-const destFile = path.resolve(process.cwd(), 'cli.js');
-console.log(`${sourceFile} -> ${destFile}`)
-fs.copyFile(sourceFile, destFile, (err) => {
-    if (err) {
-        console.error('Erro ao copiar o arquivo cli.js:', err);
-    } else {
-        console.log('Arquivo cli.js copiado com sucesso.');
-    }
->>>>>>> 7a7339d8af9a2228acf0d620d15f2daca4e838d5
 });
